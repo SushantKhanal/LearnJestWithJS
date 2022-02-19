a56b1c3b9cfc94afde0ccaef3e98ba7d9982d062
SushantKhanal <dreamerking114@gmail.com>
2022-02-19 12:53:07 -0600

    test for async await

--- functions.test.js
+++ functions.test.js
@@ -41,10 +41,17 @@ test('Admin should be in usernames', () => {
 });
 
 // Working with async data
-test('User fetched name should be Leanne Graham', () =>  {
-    // expect.assertions(1);
-    return functions.fetchUser()
-        .then(data => {
-            expect(data.name).toEqual('Leanne Graham1');
-        })
+// test('User fetched name should be Leanne Graham', () =>  {
+//     // expect.assertions(1);
+//     return functions.fetchUser()
+//         .then(data => {
+//             expect(data.name).toEqual('Leanne Graham');
+//         })
+// });
+
+// Working with Async Await
+test('Async Await User fetched name should be Leanne Graham', async () =>  {
+    expect.assertions(1);
+    const data = await functions.fetchUser();
+    expect(data.name).toEqual('Leanne Graham');
 });
