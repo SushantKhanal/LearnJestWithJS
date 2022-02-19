f1bbb1122f8dc2f0819c163c424e5d71f7d4c6fb
SushantKhanal <dreamerking114@gmail.com>
2022-02-19 13:17:15 -0600

    new test suite added

--- functions.test.js
+++ functions.test.js
@@ -1,43 +1,43 @@
-const functions = require('./functions');
+const functions = require("./functions");
 
-test('Adds 2 + 2 to equal 4', () => {
-    expect(functions.add(2, 2)).toBe(4);
+test("Adds 2 + 2 to equal 4", () => {
+  expect(functions.add(2, 2)).toBe(4);
 });
 
 //toBeNull
-test('Should be Null', () => {
-    expect(functions.isNull()).toBeNull();
+test("Should be Null", () => {
+  expect(functions.isNull()).toBeNull();
 });
 
 //toBeFalsy
-test('Should be falsy', () => {
-    expect(functions.checkValue(undefined)).toBeFalsy(); //.toBeTruthy
+test("Should be falsy", () => {
+  expect(functions.checkValue(undefined)).toBeFalsy(); //.toBeTruthy
 });
 
 //toEqual unlike toBe can compare obj and arrays etc
-test('User should be Sushant Khanal Object', () => {
-    expect(functions.createUser()).toEqual({
-        firstName: 'Sushant',
-        lastName: 'Khanal'
-    });
+test("User should be Sushant Khanal Object", () => {
+  expect(functions.createUser()).toEqual({
+    firstName: "Sushant",
+    lastName: "Khanal",
+  });
 });
 
 //less than and greater than
-test('Should be under 1600', () => {
-    const load1 = 800;
-    const load2 = 800;
-    expect(load1 + load2).toBeLessThanOrEqual(1600);
+test("Should be under 1600", () => {
+  const load1 = 800;
+  const load2 = 800;
+  expect(load1 + load2).toBeLessThanOrEqual(1600);
 });
 
 // Regex
-test('There is no I in team', () => {
-    expect('team').not.toMatch(/I/i);
+test("There is no I in team", () => {
+  expect("team").not.toMatch(/I/i);
 });
 
 // Arrays
-test('Admin should be in usernames', () => {
-    usernames = ['john', 'karen', 'admin'];
-    expect(usernames).toContain('admin');
+test("Admin should be in usernames", () => {
+  usernames = ["john", "karen", "admin"];
+  expect(usernames).toContain("admin");
 });
 
 // Working with async data
@@ -50,8 +50,8 @@ test('Admin should be in usernames', () => {
 // });
 
 // Working with Async Await
-test('Async Await User fetched name should be Leanne Graham', async () =>  {
-    expect.assertions(1);
-    const data = await functions.fetchUser();
-    expect(data.name).toEqual('Leanne Graham');
+test("Async Await User fetched name should be Leanne Graham", async () => {
+  expect.assertions(1);
+  const data = await functions.fetchUser();
+  expect(data.name).toEqual("Leanne Graham");
 });
